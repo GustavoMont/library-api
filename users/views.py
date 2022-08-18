250520548284b2482e33987ced53c78ed773907a
Luis Gustavo <lgmluisgm@gmail.com>
2022-08-18 17:50:40 -0300

    :pencil: create serializers and views to users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import DjangoModelPermissions
+from users import models, serializers
 
 # Create your views here.
+class UserViewset(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+    pagination_class = [DjangoModelPermissions]
