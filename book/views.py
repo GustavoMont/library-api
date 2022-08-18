b8db55cb818fa82d382a8328df3cebcceacfa613
Luis Gustavo <lgmluisgm@gmail.com>
2022-08-17 23:52:58 -0300

    using permissions

--- book/views.py
+++ book/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
 from book.serializers import BookSerializer
 from book.models import Book
+from rest_framework.permissions import DjangoModelPermissions
 
 # Create your views here.
 class BookViewset(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    permission_classes = [DjangoModelPermissions]
