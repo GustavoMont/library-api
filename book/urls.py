63b7da18b93c6ff6674ba9e701ce85cac984f65e
l.monteiro1 <l.monteiro@ctrlplay.com.br>
2022-09-01 18:19:02 -0300

    :pencil: prevent user see unavailable books

--- book/urls.py
+++ book/urls.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from book.views import BookViewset
 
 router = routers.DefaultRouter()
-router.register(r'books', BookViewset)
+router.register(r'books', BookViewset, basename='books')
--- book/views.py
+++ book/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from rest_framework import viewsets
 from book.serializers import BookSerializer
 from book.models import Book
@@ -5,6 +6,15 @@ from permissions.books import BookPermission
 
 # Create your views here.
 class BookViewset(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
+    # queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [BookPermission]
+
+    def get_queryset(self):
+        if self.request.user.id:
+            user_group = list(self.request.user.groups.values())[0]
+            if 'ADMIN' in user_group['name']:
+                return Book.objects.all() 
+        print(Book.objects.filter(available = True))
+        return Book.objects.filter(quantity__gt = 0, available = True)
+
