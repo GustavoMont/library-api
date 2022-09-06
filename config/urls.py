65f703680ca8562ee165365b259ce56fddc50eda
Luis Gustavo <lgmluisgm@gmail.com>
2022-09-06 00:21:30 -0300

    :tada: add swagger to api

--- config/urls.py
+++ config/urls.py
@@ -6,10 +6,26 @@ from loans.urls import router as loan_router
 from users.views import CreateUserView, MyTokenObtainPairView
 from rest_framework_simplejwt.views import (TokenRefreshView)
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Library API",
+        default_version='1.0.0',
+        description="API documentation of App",
+    ),
+    public=True,
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls, name='Admin'),
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
+    path('auth/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/register/', CreateUserView.as_view(), name="register"),
     path('', include(loan_router.urls), name='loans'),
     path('', include(user_router.urls), name='users'),
